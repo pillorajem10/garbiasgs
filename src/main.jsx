6ccc9ddb78b18d2224454eadde58fcc068761b6e
Jem Pillora <pillorajem10@gmail.com>
2026-05-15 20:25:02 +0800

    Fixed website responsiveness

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./styles/sections.css";
 import App from "./App.jsx";
 import AppErrorBoundary from "@components/AppErrorBoundary";
 
